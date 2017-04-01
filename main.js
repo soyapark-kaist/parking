328dd64a6d8dd569fec7a1a66802874b400c61dd
soyapark <soya@kaist.ac.kr>
2017-04-02 05:31:12 +0900

    Add major files

--- main.js
+++ main.js
@@ -13,10 +13,11 @@
 
 'use strict';
 
-var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAl0156r5WQzUBqxLnl1grNvWs-D6W629w';
+var currentImg, currentImgUrl;
+var questions = [];
 
 $(function() {
-    var storageRef = storage.ref();
+
 
     var playersRef = firebase.database().ref('images/');
     // Attach an asynchronous callback to read the data at our posts reference
@@ -26,22 +27,37 @@ $(function() {
         for (var o in imgs) {
             if (imgs[o].status == 2) continue;
 
-            var imgRef = storageRef.child('images/' + o);
-            // call .then() on the promise returned to get the value
-            imgRef.getDownloadURL().then(function(url) {
-                var pulledProfileImage = url;
-                $("#myimg").attr("src", url);
-            });
 
-            if (imgs[o].status == 0)
-                sendFileToCloudVision("gs://rally-parking.appspot.com/images/" + o);
-
+            questions.push({ id: o, data: imgs[o] });
             //remember this index and resume with index+1
-
-            break;
         }
+
+        fetchQuestion();
     });
 });
+
+function fetchQuestion() {
+    debugger;
+    var q = questions.shift();
+    currentImg = q.id;
+    var storageRef = storage.ref();
+    var imgRef = storageRef.child('images/' + q.id);
+    // call .then() on the promise returned to get the value
+    imgRef.getDownloadURL().then(function(url) {
+
+        currentImgUrl = url;
+        $("#myimg").attr("src", currentImgUrl);
+
+        if (q.data.status == -1) // this image just loaded
+        //request to cloud vision.
+            sendFileToCloudVision(currentImgUrl);
+        else {
+            //load text from db
+            loadText(currentImg);
+        }
+    });
+
+}
 
 /**
  * 'submit' event handler - reads the image bytes and sends it to the Cloud
@@ -64,75 +80,125 @@ function uploadFiles(event) {
  * Sends the given file contents to the Cloud Vision API and outputs the
  * results.
  */
-function sendFileToCloudVision() {
-    var request = {
-        requests: [{
-            image: {
-                source: {
-                    // "gcsImageUri": inURL
-                    "gcsImageUri": "gs://rally-parking.appspot.com/images/2017-3-25nVuBX"
-                }
-            },
-            features: [{
-                type: "TEXT_DETECTION",
-                maxResults: 200
-            }]
-        }]
-    };
+function sendFileToCloudVision(inUrl) {
+    $("#results").text("loading");
+    //Prepare form data
+    var formData = new FormData();
+    // formData.append("file", fileToUpload);
+    formData.append("url", inUrl);
+    formData.append("language", "kor");
+    formData.append("apikey", "aea9bfed2588957");
+    formData.append("isOverlayRequired", true);
 
-    $('#results').text('Loading...');
-    $.post({
-        url: CV_URL,
-        data: JSON.stringify(request),
-        contentType: 'application/json'
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
-    }).done(displayJSON);
-}
+    //Send OCR Parsing request asynchronously
+    jQuery.ajax({
+        url: "https://api.ocr.space/parse/image",
+        data: formData,
+        dataType: 'json',
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(ocrParsedResult) {
+            //Get the parsed results, exit code and error message and details
+            var parsedResults = ocrParsedResult["ParsedResults"];
+            var ocrExitCode = ocrParsedResult["OCRExitCode"];
+            var isErroredOnProcessing = ocrParsedResult["IsErroredOnProcessing"];
+            var errorMessage = ocrParsedResult["ErrorMessage"];
+            var errorDetails = ocrParsedResult["ErrorDetails"];
+            var processingTimeInMilliseconds = ocrParsedResult["ProcessingTimeInMilliseconds"];
 
-/**
- * Displays the results.
- */
-function displayJSON(data) {
-    var contents = data["responses"][0]["fullTextAnnotation"].text;
-    $('#results').text(contents);
-    var evt = new Event('results-displayed');
-    evt.results = contents;
-    document.dispatchEvent(evt);
-}
+            //If we have got parsed results, then loop over the results to do something
+            if (parsedResults != null) {
 
-function correctPlate() {
-    var pRef = firebase.database().ref("images/" + inFilename);
+                //Loop through the parsed results
+                $.each(parsedResults, function(index, value) {
+                    var exitCode = value["FileParseExitCode"];
+                    var parsedText = value["ParsedText"];
+                    var errorMessage = value["ParsedTextFileName"];
+                    var errorDetails = value["ErrorDetails"];
 
-    pRef.update({
-        "time-line": {
-            "submit": p["time-line"]["submit"],
-            "sent": new Date().toString()
-        }
-    }, function(error) {
-        if (error) {
-            console.log(error);
-        } else {
-            alert("감사합니다!");
+                    console.log(parsedText);
+                    $('#results').text(parsedText);
+
+                    var textOverlay = value["TextOverlay"];
+                    var pageText = '';
+                    switch (+exitCode) {
+                        case 1:
+                            pageText = parsedText;
+                            break;
+                        case 0:
+                        case -10:
+                        case -20:
+                        case -30:
+                        case -99:
+                        default:
+                            pageText += "Error: " + errorMessage;
+                            break;
+                    }
+                });
+            }
+        },
+        fail: function(data) {
+
         }
     });
 }
 
-function updatePlate() {
-    var pRef = firebase.database().ref("images/" + inFilename);
 
-    // reset status to 0 & update with new answer
+function correctPlate() {
+    var pRef = firebase.database().ref("images/" + currentImg);
+
+    pRef.once("value").then(function(snapshot) {
+        pRef.update({
+            "status": snapshot.val().status + 1,
+            "time": new Date().toString()
+        }, function(error) {
+            if (error) {
+                console.log(error);
+            } else {
+                alert("다음 문제로 넘어갑니다");
+                fetchQuestion();
+                // go to next question. 
+            }
+        });
+    })
+
+
+
+    return false;
+}
+
+
+function updatePlate(inValue) {
+    var pRef = firebase.database().ref("images/" + currentImg);
+
     pRef.update({
         "status": 0,
-        "text": $(".wrong  input").val()
+        "text": inValue ? inValue : $(".wrong  input").val(),
+        "time": new Date().toString()
     }, function(error) {
         if (error) {
             console.log(error);
         } else {
-            alert("감사합니다!");
+            alert("다음 문제로 넘어갑니다");
+            fetchQuestion();
             // go to next question. 
         }
     });
+
+    return false;
+}
+
+/**
+ * Displays the results if any.
+ */
+function loadText() {
+    var pRef = firebase.database().ref("images/" + currentImg);
+
+    pRef.once("value").then(function(snapshot) {
+        $("#results").text(snapshot.val().text);
+    })
 
     return false;
 }
